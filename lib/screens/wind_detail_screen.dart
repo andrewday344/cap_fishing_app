7a17ace5900f2c5a7c8c1805082e2b97dadc49db
Andrew.Day344 <andrew.day344@gmail.com>
2026-02-14 10:34:30 +1030

    Take 16a - try to fix API issue not returning data from WillyWeather

--- lib/screens/wind_detail_screen.dart
+++ lib/screens/wind_detail_screen.dart
@@ -63,7 +63,7 @@ class WindDetailScreen extends StatelessWidget {
                     return ListTile(
                       leading: Text(timeLabel, 
                         style: const TextStyle(fontWeight: FontWeight.bold)),
-                      title: Text("$speedKnots kts", 
+                      title: Text("$speedKnots ktss", 
                         style: const TextStyle(fontSize: 18, fontWeight: FontWeight.w500)),
                       subtitle: Text(entry['directionText'] ?? ""),
                       trailing: _getWindIcon(speedKnots),
